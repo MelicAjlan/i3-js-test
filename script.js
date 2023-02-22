0235a12f2b2dbe46bcfe36b11a9d5adbea3fc824
MelicAjlan <123817879+MelicAjlan@users.noreply.github.com>
2023-02-23 00:25:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -91,9 +91,9 @@ submitBtn.addEventListener("click", () => {
             loadQuiz();
         } else {
             quiz.innerHTML = `
-                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                <h2>Odgovorili ste točno na ${score}/${quizData.length} pitanja.</h2>
                 
-                <button onclick="location.reload()">Reload</button>
+                <button onclick="location.reload()">Ponovi</button>
             `;
         }
     }
